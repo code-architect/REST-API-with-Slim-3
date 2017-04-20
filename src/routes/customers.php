2ee5449c1843678a47a48b5d14983718c1d56c8b
code-architect <codearchitectdeveloper@gmail.com>
2017-04-20 13:55:45 +0530

    fetching a single customer data

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -12,8 +12,32 @@ $app->get('/api/customers', function(Request $request, Response $response){
         $stmt = $db->query($sql);
         $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
         $db = null;
-        
+
         echo json_encode($customers);
+
+    }catch (PDOException $e)
+    {
+        echo '{"error" : {"text": '.$e->getMessage().'}}';
+    }
+
+});
+
+
+// Get a single customer data
+$app->get('/api/customer/{id}', function(Request $request, Response $response){
+    $id = $request->getAttribute('id');
+
+    $sql = "SELECT * FROM customers WHERE id = :id";
+
+    try{
+        $db = DB::connect();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $customer = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+
+        echo json_encode($customer);
 
     }catch (PDOException $e)
     {
